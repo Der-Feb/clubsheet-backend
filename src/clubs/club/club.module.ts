8b26f81ad3d5f76a69815cdfe244d69cc5ccbf03
Der-Feb <bugingoderfeb@gmail.com>
2026-09-09 11:31:16 +0200

    feat: adding the upload mechanism to the club

--- src/clubs/club/club.module.ts
+++ src/clubs/club/club.module.ts
@@ -3,10 +3,19 @@ import { AuditLogsModule } from '@infrastructure/audit-logs/audit-logs.module';
 import { ClubService } from './club.service';
 import { ClubResolver } from './club.resolver';
 import { AuthModule } from '@iam/auth/auth.module';
+import { CloudinaryUploadInterceptor } from '../../media/cloudinary/cloudinary.interceptor';
+import { CloudinaryService } from '../../media/cloudinary/cloudinary.service';
+import { CloudinaryProvider } from '../../media/cloudinary/cloudinary.provider';
 
 @Module({
   imports: [AuditLogsModule, AuthModule],
-  providers: [ClubService, ClubResolver],
+  providers: [
+    ClubService, 
+    ClubResolver, 
+    CloudinaryUploadInterceptor, 
+    CloudinaryService, 
+    CloudinaryProvider
+  ],
   exports: [ClubService],
 })
 export class ClubModule {}
--- src/clubs/club/club.resolver.ts
+++ src/clubs/club/club.resolver.ts
@@ -1,5 +1,5 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
-import { UseGuards } from '@nestjs/common';
+import { UseGuards, UseInterceptors } from '@nestjs/common';
 import { ClubService } from './club.service';
 import { Club } from '@generated/prisma-nestjs-graphql/club/club.model';
 import { CreateClubInput, UpdateClubInput } from './club.dto';
@@ -13,6 +13,7 @@ import { PermissionsGuard } from '@common/guards/permissions.guard';
 import { RequirePermissions } from '@common/decorators/require-permissions.decorator';
 import { CurrentMembership, CurrentUser } from '@common/decorators/current-user';
 import { TUserJWTPayload } from '@iam/auth/strategy/jwt.strategy';
+import { CloudinaryUploadInterceptor } from '../../media/cloudinary/cloudinary.interceptor';
 
 @Resolver(() => Club)
 @UseGuards(PassportJwtGuard, EmailVerifiedGuard)
@@ -33,6 +34,7 @@ export class ClubResolver {
   }
 
   @Mutation(() => Club, { name: 'createClub' })
+  @UseInterceptors(CloudinaryUploadInterceptor)
   async createClub(
     @Args('input') input: CreateClubInput,
     @CurrentUser() currentUser: TUserJWTPayload,
@@ -41,6 +43,7 @@ export class ClubResolver {
   }
 
   @Mutation(() => Club, { name: 'updateClub' })
+  @UseInterceptors(CloudinaryUploadInterceptor)
   @UseGuards(ActiveMembershipGuard, PermissionsGuard)
   @RequirePermissions(true, ['CLUB_WRITE'])
   async updateClub(
